f61d7727954849d95f63e2753ed3f5d579b84d0b
shahz <shahz@DESKTOP-VF28LMH>
2016-07-22 16:04:38 -0400

    copied everything from github because of currupted window

--- assignment 4/Program.cs
+++ assignment 4/Program.cs
@@ -10,6 +10,36 @@ namespace assignment_4
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int[] diceRolls = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+
+            int[] tally = new int[13];
+            int maxRolls = 36000;
+
+            // builds the array
+            for (int index = 0; index < maxRolls; index++)
+            {
+                int firstDie = random.Next(1, 7);
+                int secondDie = random.Next(1, 7);
+                int dice = firstDie + secondDie;
+                tally[dice]++;
+                /**
+                 * this loop is to display dice rolls by 
+                 * removing the comments the loop is covered in
+                 * foreach (int result in diceRolls)
+                {
+                    int roll = diceRolls[dice];
+                    Console.Write(roll+" ");
+                }
+                */
+            }
+            for (int index = 2; index < 13; index++)
+            {
+                Console.WriteLine("{0}: {1} ", index, tally[index]);
+            }
+
+
         }
     }
 }
+
